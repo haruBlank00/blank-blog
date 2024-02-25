38f8c2c8e7d6c270d44beacef02db2a30149bdcd
haruBlank <harublank01@gmail.com>
2024-02-25 12:17:51 +0545

    setup remix and shadcn adn tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -8,8 +8,10 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import stylesheet from "~/tailwind.css";
 
 export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
@@ -23,6 +25,9 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <h1 className="text-red-500 text-3xl font-bold underline">
+          Hello world!
+        </h1>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,8 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ["**/*.css"],
+  tailwind: true,
+  postcss: true,
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
